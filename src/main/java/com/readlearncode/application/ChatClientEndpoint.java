b23ae7416c993f14797c0cbb9ca9bb22dc17ef6a
Yaser <yaser.ghaderipour@gmail.com>
2021-02-25 19:53:44 +0330

    complete client server code

--- src/main/java/com/readlearncode/application/ChatClientEndpoint.java
+++ src/main/java/com/readlearncode/application/ChatClientEndpoint.java
@@ -1,7 +1,11 @@
 package com.readlearncode.application;
 
 import com.readlearncode.domain.Message;
+import com.readlearncode.infrastructure.MessageDecoder;
+import com.readlearncode.infrastructure.MessageEncoder;
 
+import javax.websocket.ClientEndpoint;
+import javax.websocket.OnMessage;
 import java.text.SimpleDateFormat;
 
 /**
@@ -9,11 +13,13 @@ import java.text.SimpleDateFormat;
  * @version 1.0
  */
 // TODO Add client endpoint annotation
+@ClientEndpoint(encoders = MessageEncoder.class, decoders = MessageDecoder.class)
 public class ChatClientEndpoint {
 
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
 
     // TODO: add OnMessage annotation.
+    @OnMessage
     public void onMessage(Message message) {
         System.out.println(String.format("[%s:%s] %s",
                 simpleDateFormat.format(message.getReceived()), message.getSender(), message.getContent()));
